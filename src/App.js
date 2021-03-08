6dfef5c12faf5940a7fd33b8df884a0fdc588144
gabar <gabriel_santos9@hotmail.com>
2021-03-09 00:03:37 +0100

    add material

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello, world!
+      </h1>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+    </div>
+  );
+}
+
+export default App;
